c93bae20591c6ae5ac530e03470b7d57e93cc31c
Ashley Igonia <i.ashley0330@gmail.com>
2025-10-21 10:09:05 +0800

    backup-branch

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,4 @@
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput
-
-# Run migrations
-python manage.py migrate --noinput
+python3.9 manage.py collectstatic --noinput --clear
